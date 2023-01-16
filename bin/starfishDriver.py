6b8030ed5e940ae4e543f00f1d53315f07356bc2
nickeener <36525811+nickeener@users.noreply.github.com>
2023-01-16 12:32:46 -0800

    Remove z plane reduction for reference image

--- bin/starfishDriver.py
+++ bin/starfishDriver.py
@@ -527,12 +527,12 @@ def run(
 
         ref_img = None
         if use_ref:
-            ref_img = img.reduce({Axes.CH, Axes.ROUND, Axes.ZPLANE}, func="max")
+            ref_img = img.reduce({Axes.CH, Axes.ROUND}, func="max")
         if anchor_name:
             ref_img = (
                 experiment[fov]
                 .get_image(anchor_name)
-                .reduce({Axes.CH, Axes.ROUND, Axes.ZPLANE}, func="max")
+                .reduce({Axes.CH, Axes.ROUND}, func="max")
             )
 
         if rescale:
